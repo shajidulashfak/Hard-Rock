03717395e06fc63af86153e4914f0c34ee89b61c
shajidulashfak <shajidulashfak@gmail.com>
2021-02-09 00:06:47 +0600

    half done

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-alert('hello')
+const searchSong = () =>{
+    const searchText = document.getElementById('search-field').value;
+    const url = `https://api.lyrics.ovh/suggest/${searchText}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displaySong(data.data))
+}
+
+const displaySong = song => {
+    const songContainer = document.getElementById('song-container');
+    song.forEach(song =>{
+        const songDiv = document.createElement('div');
+        songDiv.className = 'single-result row align-items-center my-3 p-3';
+        songDiv.innerHTML = ` 
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">Album by <span>${song.artist.name}</span></p>
+            <audio controls>
+                <source src="${song.preview}" type="audio/mpeg">
+            </audio>
+            
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+            <button onclick="getLyric('${song.artist.name}', '${song.title}')" class="btn btn-success">Get Lyrics</button>
+        </div>`;
+        songContainer.appendChild(songDiv);
+    });
+}
+
+const getLyric = (artist, title) => {
+    console.log(artist, title);
+}
